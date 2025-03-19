51fbcf2c7ba885de9f15f7123732836942e7c887
Eliver <Eliver52@outlook.com>
2025-03-19 17:53:12 +0800

    初始化仓库

--- src/services/__init__.py
+++
@@ -1,8 +0,0 @@
-from .ai.llm_service import LLMService
-from .ai.image_recognition_service import ImageRecognitionService
-
-__all__ = [
-    'LLMService', 'ImageRecognitionService'
-]
-
-# 空文件，标记为Python包
